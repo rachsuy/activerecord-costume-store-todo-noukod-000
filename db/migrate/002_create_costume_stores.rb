05c52b236de400c4201c2b0c2e6b78e19437daf8
Rachele Suy <rachelesuy@gmail.com>
2019-06-24 16:50:48 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costume_stores do|value|
+        value.string :name
+        value.string :location
+        value.integer :number_of_costumes
+        value.integer :aumber_of_employees
+        value.string :whether
+        value.datetime :opening_time
+        value.datetime :closing_time
+      end
+    end
+  end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :haunted_houses do |value|
+      value.string :name
+      value.string :location
+      value.string :theme
+      value.float :price
+      value.string :family_friendly
+      value.datetime :opening_date
+      value.datetime :closing_date
+      value.string :long_description
+    end
+  end
+end
